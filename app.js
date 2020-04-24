e270c85797d4122724d02ab0058c61ef46d3beb5
NguyenThaiTiep <nguyenthaitiep206@gmail.com>
2020-04-24 23:34:38 +0700

    nocmt

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var db = require('./Database');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
