5d67c7a07fddeb36873f401639d587de68467668
NguyenThaiTiep <nguyenthaitiep206@gmail.com>
2020-04-25 03:34:39 +0700

    destroy mysql

--- Database.js
+++ Database.js
@@ -1,19 +1,32 @@
 const sql = require('mysql');
 var mysql = require('mysql');
 
-
-var conn = mysql.createConnection({
+var pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 });
-//kết nối.
-conn.connect(function(err) {
-    //nếu có nỗi thì in ra
-    if (err) throw err.stack;
-    //nếu thành công
-    console.log('connect database success');
+
+pool.getConnection(function(err, connection) {
+    console.log(err);
+
+    connection.destroy();
 });
 
-module.exports = conn;
+// var conn = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// });
+
+//kết nối.
+// conn.connect(function(err) {
+//     //nếu có nỗi thì in ra
+//     if (err) throw err.stack;
+//     //nếu thành công
+//     console.log('connect database success');
+// });
+
+module.exports = pool;
