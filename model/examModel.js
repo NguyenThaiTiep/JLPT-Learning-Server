8e57a32ed6b66eecdc8dfeff8293be5c8726cb1a
NguyenThaiTiep <nguyenthaitiep206@gmail.com>
2020-04-28 14:31:08 +0700

    change database

--- model/examModel.js
+++ model/examModel.js
@@ -138,33 +138,7 @@ module.exports.add = async(req, res) => {
         var b = add(id);
     }
 
-    // function uploadFile(file) {
-    //     var form = new formidable.IncomingForm();
-    //     form.uploadDir = "public/images";
-    //     form.keepExtensions = true;
-    //     form.maxFieldsSize = 10 * 1024 * 1024;
-    //     form.multiples = true;
-    //     form.parse((err, fields, files) => {
-    //         if (err) {
-    //             res.json({
-    //                 result: "failed",
-    //                 data: {},
-    //                 message: "Cannot upload image.error"
-    //             })
-    //             return;
-    //         }
-    //         var imgs = files[""];
 
-    //         var fileNames = [];
-
-    //         if (imgs.path) fileNames.push(imgs.path.split("\\").pop())
-    //         if (imgs.length >= 0) {
-    //             imgs.forEach(eachFile => {
-    //                 fileNames.push(eachFile.path.split("\\").pop())
-    //             });
-    //         }
-    //     })
-    // }
     res.sendStatus(200);
 }
 
